4113ec85eda692abd9ae0680aef6050d3546ed0f
HomeComp <kuzucucihan@gmail.com>
2025-02-09 01:20:58 +0300

    class, psvm, sout

--- src/main/java/com/cihankuzucu/_1_part_javatypes/_01_JavaTypes.java
+++ src/main/java/com/cihankuzucu/_1_part_javatypes/_01_JavaTypes.java
@@ -1,4 +1,15 @@
 package com.cihankuzucu._1_part_javatypes;
 
+//Single Comment
+
+/*Multiple
+Comment*/
+
 public class _01_JavaTypes {
-}
+    //    psvm + tab
+    public static void main(String[] args) {
+        // Çıktı
+        //sout + tab
+        System.out.println("Bismillah");
+    } //end psvm
+} // end class
